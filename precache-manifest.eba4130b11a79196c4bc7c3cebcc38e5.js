c4b8178af31e175e0c03e01bef30a2a8169a8f5a
Reynaldo <reynaldob.webdev@gmail.com>
2019-05-29 09:19:24 -0400

    Updates

---
+++ precache-manifest.eba4130b11a79196c4bc7c3cebcc38e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e0ee88d225c5026337429ee64650bb",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8812e65496bf19304001",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8812e65496bf19304001",
+    "url": "/contactmanager/static/js/2.cd0e2d2f.chunk.js"
+  },
+  {
+    "revision": "2f15f7a9201061021650",
+    "url": "/contactmanager/static/js/main.a1cccda7.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
